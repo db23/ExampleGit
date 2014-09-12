8d44732d115d23ca54f4f5c29aadd52a41faf17f
LFilips <luca.filipponi89@gmail.com>
2014-09-12 22:46:51 +0200

    Commit to fork

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,8 @@ public class Main {
         System.out.println("My first Git Example");
         System.out.println("My second Git Example");
         System.out.println("My Thrird Git Example");
+
+
+        System.out.println("My fourth ");
     }
 }
