f9026eccbc6ec1f78427593eec1809cf8c5f30fc
db23 <danilo@MacBook-di-Danilo.local>
2014-09-12 22:11:31 +0200

    Second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("My first Git Example");
+        System.out.println("My second Git Example");
     }
 }
